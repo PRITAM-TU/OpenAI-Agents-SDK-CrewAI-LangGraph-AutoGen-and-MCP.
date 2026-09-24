02923f6c2ff17959edd644cdabbe87a7fdec091e
Pritam Tung <pritamtung03@gmail.com>
2026-09-24 15:18:54 +0530

    Setup New project Deep Research

---
+++ Deep_Research_App/main.py
@@ -0,0 +1,6 @@
+from agents import Agent, WebSearchTool, trace, Runner, function_tool
+from agents.model_settings import ModelSettings
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+import asyncio
+from IPython.display import display, Markdown
